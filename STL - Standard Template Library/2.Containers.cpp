02f1175b1d278204112a0ad38b038f89b6fb1a45
nikhil051102 <nikhildeore05@gmail.com>
2022-01-06 21:39:49 +0530

    -

---
+++ STL - Standard Template Library/2.Containers.cpp
@@ -0,0 +1,20 @@
+/*
+There are 3 types of Container :
+1) Sequence Container :- Object which stores data in 'Sequence Fashion'. ex. 1)Vector  2)List  3)Dequeue
+2) Associative Container :- Object which gives 'Direct Access' to stored data. ex. 1)Set 2)Multiset 3)Map 4)Multimap
+3) Derived Container :- Related to Real World Modelling. ex.1)Stack 2)Queue
+
+
+When to use which ?
+1) Sequence Container :-
+                a)Vector : Random Access - Fast
+                           Middle Insertion/Deletion - Slow
+                           Insertion/Deletion at the End - Fast
+                b)List : Random Access - Slow
+                         Middle Insetion/Deletion - Fast
+                         Insertion/Deletion at the End - Fast
+
+2) Associative Sequence :- All Operation are Fast except Random Access.
+
+3) Derived Sequence :- Depends.
+*/
---
+++ STL - Standard Template Library/3.Vectors.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// Documentation : https://www.cplusplus.com/reference/vector/vector/ 
+
+template <class T>
+void Display(vector<T> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    vector<int> vec1;                   //Zero length integer vector.
+    vector<char> vec2(10);               //10-element character vector.
+    vector<char> vec3(vec2);            //vec3 is derived from vec2.
+    vector<float> vec4(4, 3.4);         //3.4 will be printed 4 times.
+    int n;
+    cout<<"Size of Vector : ";
+    cin>>n;
+
+    int element;
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"Element "<<i+1<<": ";
+        cin>>element;
+        vec1.push_back(element);         //To insert element at the end of vector.
+    }
+    // vec1.pop_back();                    //This function will remove last element of vector.
+    Display(vec1);
+
+
+    //Insertion of Element in Vector
+    vector<int> :: iterator iter = vec1.begin();
+    vec1.insert(iter+1, 200);             //Inserting 200 at the 2nd Position.
+    Display(vec1);
+
+    //Inserting multiple copies of element in Vector
+    vec1.insert(iter+3, 5, 100);          //Inserting 5 copies of 100 at the 4th Position
+    Display(vec1);
+    return 0;
+}
